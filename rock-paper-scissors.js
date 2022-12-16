0989bc87f38a04f25aeedc78b0a77d25e90f73a3
Capra Irata <113552738+capra-irata@users.noreply.github.com>
2022-12-16 14:59:57 -0800

    Remove pseudocode and outline from top of file

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,24 +1,3 @@
-/*
-PSEUDOCODE
-
-Get choice input from player (rock, paper, or scissors).
-  Make input case insensitive.
-Randomly get choice from computer.
-Determine whether player wins or loses.
-Print win or loss declaration on screen with results of choices.
-Repeat game for 5 rounds.
-  Keep score of total rounds won and lost by player.
-  Report whether the player wins or loses the overall game.
-
-OUTLINE
-
-function getPlayerChoice()
-function getComputerChoice()
-function printResults()
-function playRound()
-function game()
-*/
-
 function getComputerChoice() {
   // Generate a random integer inclusively between 0 and 2
   switch (Math.floor(Math.random() * 3)) {
